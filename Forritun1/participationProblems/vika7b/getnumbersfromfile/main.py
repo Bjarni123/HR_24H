cf02470e9e42780f3f5019872b7624d4f229e8f8
Bjarni123 <bjarni04@gmail.com>
2024-10-03 14:32:11 +0000

    python week 7b

---
+++ Forritun1/participationProblems/vika7b/getnumbersfromfile/main.py
@@ -0,0 +1,62 @@
+def main():
+    with get_file() as file_obj:
+        numbers = get_numbers_from_file(file_obj)
+        display_numbers(numbers)
+
+
+def get_file():
+    """Gets file name from user and returns the open file handle.
+
+    Asks the user repeatedly for file name
+    until an existing file name is given.
+    """
+
+    keep_asking = True
+    while keep_asking:
+        filename = input()
+        file_content = open_file(f'./src/{filename}')
+        if file_content == None:
+            print(f"{filename} not found! Please try again.")
+        else:
+            keep_asking = False
+            return file_content
+
+    # and implement this function.
+    # You can call the following function in your implementation.
+
+
+def open_file(filename: str):
+    try:
+        return open(filename)
+    except FileNotFoundError:
+        return None
+
+
+def get_numbers_from_file(file) -> list:
+    """Returns a list of all numbers in the given file.
+
+    Assumes all numbers appear on their own,
+    separated from other text by whitespace.
+    """
+
+    number_list = []
+
+    for line in file:
+        for element in line.split():
+            try:
+                number_list.append(int(element))
+            except:
+                pass
+
+    return number_list
+
+
+def display_numbers(numbers):
+    """Prints out the list, after sorting it."""
+
+    print(sorted(numbers))
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Forritun1/participationProblems/vika7b/music/starter_code.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def main():
+    # Simple test script.
+    sys.stderr.write("Input music, group, singer: ")
+    music, group, singer = input().split(",")
+
+    state_music_opinion(music, group, singer)
+    print()
+    state_music_opinion()
+    state_music_opinion("", "bjarni", "")
+
+def state_music_opinion(genre = None, music_group = None, vocalist = None):
+    if genre == None or len(genre) == 0:
+        genre = "Classic Rock"
+    if music_group == None or len(music_group) == 0:
+        music_group = "The Beatles"
+    if vocalist == None or len(vocalist) == 0:
+        vocalist = "Freddie Mercury"
+
+    print(f"The best type of music is {genre}.\nThe best music group is {music_group}.\nThe best lead vocalist is {vocalist}.")
+
+
+# Definition for state_music_opinion goes here.
+
+
+if __name__ == "__main__":
+    main()
